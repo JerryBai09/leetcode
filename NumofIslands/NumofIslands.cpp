f7dc389b81f02a7e520308cfae5d2ee7241fc885
JerryBai <davidbai2013@gmail.com>
2019-11-17 16:48:40 +0800

    Add an accept solution of Problem Num of Islands

--- NumofIslands/NumofIslands.cpp
+++ NumofIslands/NumofIslands.cpp
@@ -2,66 +2,109 @@
 // Created by JerryBai on 2019-11-17.
 //
 
+/*******************************************************************************************************
+ * Given a 2d grid map of '1's (land) and '0's (water), count the number of islands.
+ * An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically.
+ * ou may assume all four edges of the grid are all surrounded by water.
+ *
+ * Example 1:
+ *
+ * Input:
+ * 11110
+ * 11010
+ * 11000
+ * 00000
+ *
+ * Output: 1
+ * Example 2:
+ *
+ * Input:
+ * 11000
+ * 11000
+ * 00100
+ * 00011
+ *
+ * Output: 3
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/number-of-islands
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ *
+ ******************************************************************************************************/
+
+// In this problem, count the number of islands is equal to find out the number of connected areas which format by
+// all adjacent '1'
+// DFS could be used to find the answer
+//
+// The procedure of DFS:
+// 1. In main search function:
+//    Find all paths from each right start and count the paths
+// 2. In DFS function:
+//    Deal all terminate situations first and return
+//    Mark current position as visited
+//    For all four directions of current pos:
+//        do DFS for each new pos
+//
+// Complexity:
+// Time: O(mn) all elements of the grid
+// Space: O(mn) marked all position of grid if the element has been visited
+//
+
+
+
 #include <vector>
 #include <iostream>
 
 using namespace std;
 
-vector <vector<int>> directions = {{0,  1},
-                                   {0,  -1},
-                                   {1,  0},
-                                   {-1, 0}};
+vector <vector<int>> directions = {{0,  1}, {0,  -1}, {1,  0}, {-1, 0}};
 
-void search_island(vector <vector<char>> &grid, int i, int j, int& count, vector <vector<bool>> &marked) {
-    // Over the boarder of grid
-    if (i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size()) {
-        return;
-    }
-    if (grid[i][j] == '0' || marked[i][j]) {
-        marked[i][j] = true;
+void dfs(vector<vector<char>> &grid, int i, int j, vector<vector<bool>>& marked) {
+    // deal all terminate condition
+    if (i < 0 || i >= grid.size() || j < 0 || j >= grid[i].size() || marked[i][j] || grid[i][j] == '0') {
         return;
     }
 
+    // mark current position
     marked[i][j] = true;
-    bool end_flag = true;
-    for (int pos = 0; pos < directions.size(); pos++) {
-        int new_x = i + directions[pos][0];
-        int new_y = j + directions[pos][1];
-        if (new_x > 0 && new_x < grid.size() && new_y > 0 && new_y < grid[0].size() &&
-            !marked[new_x][new_y] && grid[new_x][new_y] != '0') {
-            end_flag = false;
-            search_island(grid, new_x, new_y, count, marked);
-        }
 
-    }
-    if (end_flag) {
-        cout << "Pos: " << i << " " << j << endl;
-        cout << "Num is " << count << endl;
-        count++;
+    // dfs for the adj position
+    for (auto dir : directions) {
+        int new_x = i + dir[0];
+        int new_y = j + dir[1];
+        dfs(grid, new_x, new_y, marked);
     }
 }
 
-int numIslands(vector <vector<char>> &grid) {
+int numIslands(vector<vector<char>> &grid) {
     if (grid.size() == 0) return 0;
 
     int m = grid.size();
     int n = grid[0].size();
-    vector <vector<bool>> marked(m, vector<bool>(n, false));
-
+    vector<vector<bool>> marked(m ,vector<bool>(n, false));
     int count = 0;
     for (int i = 0; i < grid.size(); i++) {
         for (int j = 0; j < grid[i].size(); j++) {
-            search_island(grid, i, j, count, marked);
+            if (grid[i][j] != '0' && !marked[i][j]) {
+                dfs(grid, i, j, marked);
+                count++;
+            }
         }
     }
 
     return count;
 }
 
-
 int main() {
     vector<vector<char>> grid1 = {{'1', '1', '1', '1', '0'}, {'1', '1', '0', '1', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '0', '0', '0'} };
-    cout << "Num of Island " << numIslands(grid1) << endl;
+    cout << "Num of Island1 " << numIslands(grid1) << endl;
+
+    vector<vector<char>> grid2 = {{'1', '1', '0', '0', '0'}, {'1', '1', '0', '0', '0'}, {'0', '0', '1', '0', '0'}, {'0', '0', '0', '1', '1'} };
+    cout << "Num of Island2 " << numIslands(grid2) << endl;
+
+    vector<vector<char>> grid3 = {{}, {}, {}, {} };
+    cout << "Num of Island3 " << numIslands(grid3) << endl;
 
     return 0;
 }
