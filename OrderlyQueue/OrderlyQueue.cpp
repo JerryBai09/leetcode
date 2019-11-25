44635341e32b0c015f2fac698859e5a959a45754
JerryBai <davidbai2013@gmail.com>
2019-11-26 00:56:41 +0800

    [Req]: Auto format comment block of problem description [Item]: Add a format script to handle this situation [TODO]: Optimize by module fileinput

--- OrderlyQueue/OrderlyQueue.cpp
+++ OrderlyQueue/OrderlyQueue.cpp
@@ -2,43 +2,45 @@
 // Created by 白俊彦 on 2019-11-22.
 //
 
-/*
-//A string S of lowercase letters is given.  Then, we may make any number of moves.
-//
-//In each move, we choose one of the first K letters (starting from the left), remove it, and place it at the end of the string.
-//
-//Return the lexicographically smallest string we could have after any number of moves.
-//
-// 
-//
-//Example 1:
-//
-//Input: S = "cba", K = 1
-//Output: "acb"
-//Explanation:
-//In the first move, we move the 1st character ("c") to the end, obtaining the string "bac".
-//In the second move, we move the 1st character ("b") to the end, obtaining the final result "acb".
-//Example 2:
-//
-//Input: S = "baaca", K = 3
-//Output: "aaabc"
-//Explanation:
-//In the first move, we move the 1st character ("b") to the end, obtaining the string "aacab".
-//In the second move, we move the 3rd character ("c") to the end, obtaining the final result "aaabc".
-// 
-//
-//Note:
-//
-//1 <= K <= S.length <= 1000
-//S consists of lowercase letters only.
-//
-//来源：力扣（LeetCode）
-//链接：https://leetcode-cn.com/problems/orderly-queue
-//著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
-*/
+/**********************************************************************
+* A string S of lowercase letters is given.  Then, we may make any number of moves.
+* 
+* In each move, we choose one of the first K letters (starting from the left), remove it, and place it at the end of the string.
+* 
+* Return the lexicographically smallest string we could have after any number of moves.
+* 
+*  
+* 
+* Example 1:
+* 
+* Input: S = "cba", K = 1
+* Output: "acb"
+* Explanation:
+* In the first move, we move the 1st character ("c") to the end, obtaining the string "bac".
+* In the second move, we move the 1st character ("b") to the end, obtaining the final result "acb".
+* Example 2:
+* 
+* Input: S = "baaca", K = 3
+* Output: "aaabc"
+* Explanation:
+* In the first move, we move the 1st character ("b") to the end, obtaining the string "aacab".
+* In the second move, we move the 3rd character ("c") to the end, obtaining the final result "aaabc".
+*  
+* 
+* Note:
+* 
+* 1 <= K <= S.length <= 1000
+* S consists of lowercase letters only.
+* 
+* 来源：力扣（LeetCode）
+* 链接：https://leetcode-cn.com/problems/orderly-queue
+* 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+*********************************************************************/
+
 
 #include <string>
 #include <iostream>
+
 using namespace std;
 
 string orderlyQueue(string S, int K) {
---
+++ Tools/format_comment.py
@@ -0,0 +1,49 @@
+#! /usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+__author__ = 'Jerry Bai'
+
+import sys
+import re
+import os
+
+
+def format(comment):
+    # match <comment> </comment>
+    content = ""
+    if comment:
+        for line in comment:
+            content += '* '
+            content += line
+            content += '\n'
+    return content
+
+
+if __name__ == "__main__":
+    with open(sys.argv[1]) as f:
+        f.seek(0)
+        text = f.read()
+        emphasis_pattern = r'<comment>([\w\W]*?)</comment>'
+        comment_mark = re.search(emphasis_pattern, text)
+        comment = []
+        if comment_mark:
+            comment = comment_mark.group(1).strip().split('\n')
+        else:
+            print("comment not found")
+        content = format(comment)
+        prefix_comment = r"/**********************************************************************\n"
+        postfix_comment = r"*********************************************************************/"
+        content = prefix_comment + content + postfix_comment
+        text = re.sub(emphasis_pattern, content, text)
+        temp_file = ""
+        if text:
+            temp_file = sys.argv[1] + "_bak"
+            with open(temp_file, 'w') as format_file:
+                format_file.write(text)
+        else:
+            print("No output text")
+
+        if temp_file:
+            os.system(r'mv %s %s' % (temp_file, sys.argv[1]))
+        else:
+            print("No format execution")
