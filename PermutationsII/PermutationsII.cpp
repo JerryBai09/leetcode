2aeaed4f4c0be96c56995147935ba0cdd51abf42
JerryBai <davidbai2013@gmail.com>
2020-01-02 01:32:48 +0800

    Feat: Init an ans for Problem Permutations II, no AC

---
+++ PermutationsII/PermutationsII.cpp
@@ -0,0 +1,63 @@
+//
+// Created by 白俊彦 on 2020-01-01.
+//
+
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+//Input: [[1,1,2]
+//Output:
+//[
+//[1,1,2],
+//[1,2,1],
+//[2,1,1]
+//]
+
+
+void backtrack(vector<int>& vec, int l, int h, vector<vector<int> >& perm) {
+    if (l == h) {
+        perm.push_back(vec);
+    }
+    else {
+        for (int i = l; i <= h; i++) {
+            if (l != i && vec[l] == vec[i]) continue;
+            swap(vec[l], vec[i]);
+            backtrack(vec, l + 1, h, perm);
+            swap(vec[l], vec[i]);
+        }
+    }
+}
+
+vector<vector<int>> permuteUnique(vector<int>& nums) {
+    vector<vector<int> > res;
+    if (nums.size() == 0) return {{}};
+
+    backtrack(nums, 0, nums.size() - 1, res);
+    return res;
+}
+
+int main() {
+    vector<int> num1;
+    vector<vector<int> > per1 = permuteUnique(num1);
+    cout << "per1" << endl;
+    for (auto vec : per1) {
+        for (auto num : vec) {
+            cout << "permutation " << num;
+        }
+        cout << endl;
+    }
+
+    vector<int> num2 = {2, 2, 1, 1};
+    vector<vector<int> > per2 = permuteUnique(num2);
+    cout << "per2" << endl;
+    for (auto vec : per2) {
+        for (auto num : vec) {
+            cout << num;
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
